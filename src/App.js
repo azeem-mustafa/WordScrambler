52362ffdefd37d72e0e546fe777c4324997fe0a1
Azeem M <87535744+azeem-mustafa@users.noreply.github.com>
2021-12-27 17:11:22 -0500

    set up basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  axios.get('https://api.hatchways.io/assessment/sentences/1').then(res => {
+    console.log(res.data)
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Hello
     </div>
   );
 }
