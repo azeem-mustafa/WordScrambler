993be95387d10bdfeb10b8f947ab29de6071b9a5
Azeem M <87535744+azeem-mustafa@users.noreply.github.com>
2021-12-30 18:22:17 -0500

    tried to make the word scrambler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import axios from 'axios';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
 
-  axios.get('https://api.hatchways.io/assessment/sentences/1').then(res => {
-    console.log(res.data)
+  const [sentence, setSentence] = useState('')
+
+  useEffect(() => {
+    axios.get('https://api.hatchways.io/assessment/sentences/1')
+      .then(res => {
+        setSentence(res.data.data.sentence)
+      })
+      .catch(err => {
+        return console.log(err)
+      })
+  }, [])
+
+  const newSentence = sentence.split(' ').forEach((words, i) => {
+    const letters = words.split('').slice(1, -1)
+  
+    // if(letters !== undefined){
+    //   letters.map((letter, i) =>{
+    //     console.log(letter)
+    //   })
+    // }
+  
+    
   })
+  console.log(newSentence)
+  
+
   return (
     <div className="App">
-     Hello
+      <h1 className='scambled-word'>
+        {sentence}
+      </h1>
+
     </div>
   );
 }
